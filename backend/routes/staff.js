8b8c18144a37bc5ea8b19672222d39ffecc7d082
snehal-810 <snehal.jadhav@sunbeaminfo.com>
2025-01-24 14:48:26 +0530

    admin login, staff registration, student registration added

--- backend/routes/staff.js
+++ backend/routes/staff.js
@@ -7,14 +7,20 @@ const jwt = require("jsonwebtoken");
 const config = require("../config");
 const authorizeRole = require("../middlewares/AuthorizeRole");
 
-const TABLE_NAME = config.STAFF_TABLE_NAME;
+const STAFF_TABLE = config.STAFF_TABLE_NAME;
+const COURSE_TABLE = config.COURSE_TABLE_NAME;
+const SUBJECT_TABLE = config.SUBJECT_TABLE_NAME;
+const SCHEMA_TABLE = config.SCHEME_TABLE_NAME;
+const GROUP_TABLE = config.GROUP_TABLE_NAME;
+const STUDENT_TABLE = config.STUDENT_TABLE_NAME;
+const MARKS_ENTRY_TABLE = config.MARK_ENTER_TABLE_NAME;
 
 // REGISTER API
 router.post("/register", (request, response) => {
     const { email, password, employee_number, staff_name } = request.body;
   
     // create a sql statement
-    const statement = `INSERT INTO ${TABLE_NAME} 
+    const statement = `INSERT INTO ${STAFF_TABLE} 
       (email, password, employee_number, staff_name)
       VALUES (?, ?, ?, ?)`;
   
